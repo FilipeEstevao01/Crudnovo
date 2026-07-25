c36b39af10d2516f2d840e98cf4e9c01f66df2f7
Filipe <filipedev0@gmail.com>
2026-07-25 19:29:31 -0300

    Aqui foi criado o metodo create

--- src/main/java/com/Crud_novo/controller/ProductController.java
+++ src/main/java/com/Crud_novo/controller/ProductController.java
@@ -1,4 +1,16 @@
 package com.Crud_novo.controller;
 
+import com.Crud_novo.entity.Product;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 public class ProductController {
+
+    @PostMapping
+    public Product salvar(@RequestBody Product product){
+        return service.salvar(product);
+    }
+
 }
